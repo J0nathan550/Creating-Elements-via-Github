c16000ca7a295e41a83b21788d379a3dac8cf184
J0nathan550 <ihudokormov@gmail.com>
2023-05-27 18:11:52 +0300

    Working smooth as butter!

--- Creating Elements via Github/MainWindow.xaml.cs
+++ Creating Elements via Github/MainWindow.xaml.cs
@@ -1,15 +1,20 @@
-﻿using System.Net.Http;
+﻿using System.Diagnostics;
+using System.Net.Http;
+using System.Timers;
 using System.Windows;
+using System.Windows.Controls;
 using Newtonsoft.Json;
 
 namespace Creating_Elements_via_Github
 {
     public partial class MainWindow : Window
     {
-        private string serverDataLink = "link";
-        private string versionApp = "0.1";
+        private string serverDataLink = "https://raw.githubusercontent.com/J0nathan550/Creating-Elements-via-Github/master/serverData.json";
+        private string versionApp = "0.2";
         private ServerData serverData = new(); 
         private HttpClient client = new();
+        private Timer update = new Timer(); 
+
         public MainWindow()
         {
             InitializeComponent();
@@ -18,9 +23,60 @@ namespace Creating_Elements_via_Github
             if (!string.IsNullOrEmpty(json))
             {
                 serverData = JsonConvert.DeserializeObject<ServerData>(json);
-                if (versionApp != serverData.ServerVersion)
+                if (serverData != null)
                 {
-                    MessageBox.Show($"New version is: {serverData.ServerVersion}", "New update of application", MessageBoxButton.OK, MessageBoxImage.Information);
+                    if (versionApp != serverData.ServerVersion)
+                    {
+                        MessageBox.Show($"New version is: {serverData.ServerVersion}", "New update of application", MessageBoxButton.OK, MessageBoxImage.Information);
+                        Process.Start(new ProcessStartInfo
+                        {
+                            FileName = serverData.GitHubLink,
+                            UseShellExecute = true
+                        });
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("There is no access to json file.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is no access to json file.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            update.Interval = 5000;
+            update.Elapsed += Update_Elapsed;
+            update.Start();
+        }
+
+        private async void Update_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            string json = client.GetStringAsync(serverDataLink).Result;
+            if (!string.IsNullOrEmpty(json))
+            {
+                serverData = JsonConvert.DeserializeObject<ServerData>(json);
+                if (serverData != null)
+                {
+                    await Dispatcher.BeginInvoke(() =>
+                    {
+                        WindowGrid.Children.Clear();
+                    });
+                    for (int i = 0; i < serverData.Buttons.Length; i++) 
+                    {
+                        await Dispatcher.BeginInvoke(() =>
+                        {
+                            Button button = new Button();
+                            button.Content = serverData.Buttons[i].Title;
+                            button.Width = 200;
+                            button.Height = 200;
+                            WindowGrid.Children.Add(button);
+                        });
+                    }
+                    serverData = new ServerData();
+                }
+                else
+                {
+                    MessageBox.Show("There is no access to json file.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
             else
@@ -32,5 +88,11 @@ namespace Creating_Elements_via_Github
     public class ServerData
     {
         public string? ServerVersion { get; set; }
+        public string? GitHubLink { get; set; }
+        public struct ButtonData
+        {
+            public string? Title { get; set; }
+        }
+        public ButtonData[]? Buttons { get; set; }
     }
 }
